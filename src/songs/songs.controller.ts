29c584bd1056c5a585b674cb50dd8ee271709d07
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-10-08 14:25:12 +0100

    testing the song controller

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
+  constructor(private songService: SongsService) {}
   @Get()
   findAll() {
-    return 'find all the songs';
+    return this.songService.findAll();
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'one song';
+  }
+
+  @Post()
+  create() {
+    return this.songService.create('If by davido');
+  }
+
+  @Put(':id')
+  update() {
+    return 'update song based on id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'deleted song based on id';
   }
 }
